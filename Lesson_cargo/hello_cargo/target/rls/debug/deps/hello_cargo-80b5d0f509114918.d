c9a3289357e655cc8b40f8f316fba1beb7af3805
Ninim Raley <poisonousheart@yandex.ru>
2022-04-12 23:27:42 +0300

    update message

---
+++ Lesson_cargo/hello_cargo/target/rls/debug/deps/hello_cargo-80b5d0f509114918.d
@@ -0,0 +1,5 @@
+c:\Git\Ninim\learn_rust\Lesson_cargo\hello_cargo\target\rls\debug\deps\hello_cargo-80b5d0f509114918.rmeta: src\main.rs
+
+c:\Git\Ninim\learn_rust\Lesson_cargo\hello_cargo\target\rls\debug\deps\hello_cargo-80b5d0f509114918.d: src\main.rs
+
+src\main.rs:
